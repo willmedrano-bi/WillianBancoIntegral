53ca798ab1de2e9b20999714b03a10267e74d556
willmedrano-bi <willian.medrano@integral.com.sv>
2026-07-22 19:30:47 -0600

    d

--- APP/index.js
+++ APP/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('!Hola desde Docker! Clase EKS 2'));
+app.get('/', (req, res) => res.send('!Hola Willian Medrano de Banco integral!'));
 app.listen(3000,'0.0.0.0', ()=> console.log('Servidor en puerto 3000'))
